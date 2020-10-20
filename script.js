ea41a432fd60e4ef52e3c1f3d791cc2988e39e5b
Alex Nordin <apnordin@gmail.com>
2020-10-19 23:33:28 -0400

    Fixed it so the app works when nothing is in localstorage

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ var citySearch = "";
 
 var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&APPID=" + APIkey;
 
-addList();
+
+
+
 
 // Create addList function to generate list of cities
 function addList() {
+
 
     citySearch = localStorage.getItem("citySearch");
 
@@ -16,12 +19,12 @@ function addList() {
     cityBtn.text(citySearch.substr(0, 1).toUpperCase() + citySearch.substr(1).replace('+', ' '));
     $(".citylist").append(cityBtn);
 
+
 }
 
 //Search button onclick with all relevant functions
 $("#search-btn").on("click", function () {
     event.preventDefault();
-    console.log('jquery is working lol');
 
     citySearch = $("#search-input").val().trim();
     citySearch = citySearch.replace(' ', '+');
@@ -43,3 +46,5 @@ $("#search-btn").on("click", function () {
 
     addList();
 })
+
+addList();
