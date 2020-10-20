573f3e25f695a7c714a91cef4ba2990f40be8cbb
Alex Nordin <apnordin@gmail.com>
2020-10-20 01:24:33 -0400

    GOT THEM COORDINATES WORKIN

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## Attempted to create a for-loop using [i] to create multiple localstorage spots for citySearch
+
+
 # 06 Server-Side APIs: Weather Dashboard
 
 Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var APIkey = "4add3f52cecb0be27920f2537ea37116";
 var queryURL = "";
 var citySearch = "";
+var today = moment();
 
 var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&APPID=" + APIkey;
 
@@ -11,16 +12,16 @@ var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch
 
 // Create addList function to generate list of cities
 function addList() {
-
-
     citySearch = localStorage.getItem("citySearch");
-
     var cityBtn = $("<button>");
     cityBtn.text(citySearch.substr(0, 1).toUpperCase() + citySearch.substr(1).replace('+', ' '));
+    cityBtn.addClass("btn btn-outline-dark my-1");
     $(".citylist").append(cityBtn);
-
-
+    $(".citylist").append($("<br>"));
 }
+
+// Create a function to append content to main card
+
 
 //Search button onclick with all relevant functions
 $("#search-btn").on("click", function () {
@@ -30,21 +31,51 @@ $("#search-btn").on("click", function () {
     citySearch = citySearch.replace(' ', '+');
     console.log('city search: ', citySearch)
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&APPID=" + APIkey;
-
-
     localStorage.setItem('citySearch', citySearch)
 
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function (response) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&APPID=" + APIkey;
 
-    //     console.log('forecast response for ' + citySearch, response);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-    // })
+        console.log('forecast response for ' + citySearch, response);
+
+        function cardContent() {
+
+            $("#city-name").text(citySearch.substr(0, 1).toUpperCase() + citySearch.substr(1).replace('+', ' ') + (moment(today).format(' (l)')));
+
+            $("#temperature").text('Temperature: ' + (Math.round((response.list[0].main.temp) * 9 / 5 - 459.67)) + "°F");
+
+            $("#humidity").text('Humidity: ' + response.list[0].main.humidity);
+
+            $("#wind-speed").text('Wind Speed: ' + response.list[0].wind.speed + ' MPH');
+
+            var lat = response.city.coord.lat;
+            var lon = response.city.coord.lon;
+
+            var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+
+            $.ajax({
+                url: uvURL,
+                method: "GET"
+            }).then(function (uvResponse) {
+
+                console.log('UV Response: ', uvResponse);
+            })
+
+            $("#uv-index").text('UV Index: ');
+
+
+        }
+
+        cardContent();
+
+    })
 
     addList();
+
 })
 
 addList();
