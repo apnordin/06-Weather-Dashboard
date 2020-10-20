44d70102b445ab0ccc571682504c3b575631f2bf
Alex Nordin <apnordin@gmail.com>
2020-10-19 22:40:33 -0400

    addList function working

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// Set initial variables
 var APIkey = "4add3f52cecb0be27920f2537ea37116";
 var queryURL = "";
 var citySearch = "";
 
 var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&APPID=" + APIkey;
 
+// Create addList function to generate list of cities
+function addList() {
+    var cityBtn = $("<button>");
+    cityBtn.text(citySearch);
+    $(".citylist").append(cityBtn);
+}
+
+//Search button onclick with all relevant functions
 $("#search-btn").on("click", function () {
     event.preventDefault();
     console.log('jquery is working lol');
@@ -14,6 +23,9 @@ $("#search-btn").on("click", function () {
 
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&APPID=" + APIkey;
 
+
+    // localStorage.setItem('')
+
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -22,4 +34,6 @@ $("#search-btn").on("click", function () {
         console.log('forecast response for ' + citySearch, response);
 
     })
+
+    addList();
 })
